818fd4d282101da97bdeaeb52f581bbbd838dad9
supawit <supawit.sak@crru.ac.th>
2015-09-25 14:45:45 +0700

    pondfapz

--- Array01.java
+++ Array01.java
@@ -1,9 +1,14 @@
+import java.util.Scanner;
 public class Array01 {
   public static void main (String args[]) {
-      int[] a;
+	  
+	  Scanner input = new Scanner(System.in);
+	  System.out.print("Insert Your Num : ");
+	  int num =input.nextInt();
+      int[] a = new int[num];
+	  for (int i = 1 ; i <= a.length ; i++){
+		  System.out.println("A = "+i);
+	  }
       
-      // Assign Value to A
-      
-      // Print a Array
   }
 }
